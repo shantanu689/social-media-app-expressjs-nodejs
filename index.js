fb4b9b53cbf9f23666806aad5e2fac2b28a82d3b
Shantanu <shantanubhai123@gmail.com>
2021-05-17 10:54:37 +0530

    added some basic files

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const port = 8080;
 
 const app = express();
 
+app.use("/", require("./routes/index"));
+
 app.listen(port, (err) => {
   if (err) {
     console.log("Error in running the server: ", err);
