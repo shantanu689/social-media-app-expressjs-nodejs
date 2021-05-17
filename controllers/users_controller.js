f35b0dc24800556d5c40b2e0f67097480e67bd58
Shantanu <shantanubhai123@gmail.com>
2021-05-17 18:02:23 +0530

    no comments

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,19 @@
 module.exports.profile = (req, res) => {
   res.end("<h1>User Profile</h1>");
 };
+
+module.exports.signUp = (req, res) => {
+  return res.render("user_sign_up", {
+    title: "the hex | Sign Up",
+  });
+};
+
+module.exports.signIn = (req, res) => {
+  return res.render("user_sign_in", {
+    title: "the hex | Sign In",
+  });
+};
+
+module.exports.create = (req, res) => {};
+
+module.exports.createSession = (req, res) => {};
