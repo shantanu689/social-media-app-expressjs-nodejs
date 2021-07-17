fbae48421517c8e45f7442f6aa1be229cd1016a8
Shantanu <shantanup101@gmail.com>
2021-07-17 15:05:13 +0530

    test

--- config/aws-config.js
+++ config/aws-config.js
@@ -1,45 +1,51 @@
-const aws = require('aws-sdk');
-const fs = require('fs')
-const env = require('../config/environment');
-const S3 = require('aws-sdk/clients/s3')
+const aws = require("aws-sdk");
+const fs = require("fs");
+const env = require("../config/environment");
+const S3 = require("aws-sdk/clients/s3");
 
-const bucketName = env.aws_bucket_name
-const region = env.aws_bucket_region
-const accessKeyId = env.aws_access_key
-const secretAccessKey = env.aws_secret_access
+const bucketName = env.aws_bucket_name;
+const region = env.aws_bucket_region;
+const accessKeyId = env.aws_access_key;
+const secretAccessKey = env.aws_secret_access;
 
 const s3 = new S3({
-    region,
-    accessKeyId,
-    secretAccessKey
-})
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
 
 // uploads a file to s3
 module.exports.uploadFile = (file) => {
-    console.log('in and file path is ', file.path)
-    const fileStream = fs.createReadStream(file.path)
-    const uploadParams = {
-        Bucket: bucketName,
-        Body: fileStream,
-        Key: file.filename
-    }
-    console.log('done and returning')
-    return s3.upload(uploadParams).promise()
-}
+  console.log("in and file path is ", file.path);
+  const fileStream = fs.createReadStream(file.path);
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.filename,
+  };
+  console.log("done and returning");
+  s3.upload(uploadParams, (err, data) => {
+    if (err) console.log(err);
+    return data;
+  });
+  // return s3.upload(uploadParams).promise()
+};
 
 // downloads a file from s3
 module.exports.getFileStream = (fileKey) => {
-    const downloadParams = {
-        Key: fileKey,
-        Bucket: bucketName
-    }
+  const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName,
+  };
 
-    return s3.getObject(downloadParams).createReadStream()
-}
+  return s3.getObject(downloadParams).createReadStream();
+};
 
 module.exports.deleteFile = (fileKey) => {
-    return s3.deleteObject({
-        Bucket: bucketName,
-        Key: fileKey
-    }).promise()
-}
+  return s3
+    .deleteObject({
+      Bucket: bucketName,
+      Key: fileKey,
+    })
+    .promise();
+};
